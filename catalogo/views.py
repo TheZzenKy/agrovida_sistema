f26541437ac719ce6869f9dff32f281e9cefa4c5
Manuel Rodriguez <manuel.rodriguez74@inacapmail.cl>
2026-09-11 17:12:35 -0300

    creación y configuracion de la función de vista en views.py.

--- catalogo/views.py
+++ catalogo/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def productos(request):
+
+    lista = [
+        {"nombre": "Fertilizante NPK 15-15-15", "categoria": "Fertilizantes", "precio": 18990, "stock": 120},
+        {"nombre": "Semilla de Maíz Híbrido", "categoria": "Semillas", "precio": 5990, "stock": 340},
+        {"nombre": "Herbicida Glifosato 20L", "categoria": "Agroquímicos", "precio": 45990, "stock": 25},
+    ]
+
+
+    return render(request, 'index.html', {'productos':productos})
